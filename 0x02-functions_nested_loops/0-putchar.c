e49be0e2296a8a5150bdba2a74a289d04f38417b
Rubise <rubisemamman@gmail.com>
2023-06-22 01:26:05 +0100

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- *main - Prints _putchar as a message
- * 
- *  
+ * main - Prints _putchar as a message
+ *
+ *
  * Return: Always 0 (Success)
-*/
+ */
 
 int main(void)
 {
