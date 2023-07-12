f2ffa29e7befad08fb87069e5028af1e4e03a603
Rubise <rubisemamman@gmail.com>
2023-07-12 12:27:19 -0700

    2 args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,8 +6,8 @@
  * Return: 0 - success.
  */
 
-int main(int argc __attribute__((unused)),__attribute__((unused)) char *argv[])
+int main(int argc, __attribute__((unused)) char *argv[])
 {
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 	return (0);
 }
