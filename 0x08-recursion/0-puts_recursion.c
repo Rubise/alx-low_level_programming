1402f9d21892661cb2571da6a01e8462b2a13889
Rubise <rubisemamman@gmail.com>
2023-07-13 05:21:42 -0700

    Puts Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,24 +1,19 @@
 #include "main.h"
-#include <unistd.h>
 
 /**
- * _puts_recursion: prints string on new line
- * _purchar: writes single character to file descriptor 1
- * s: string
- * return: none
+ * _puts_recursion - Print a string
+ * @s: string
  */
-void _putchar(char c)
-{
-	write(1, &c, 1);
-}
 
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
 		_putchar('\n');
-		return;
 	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(++s);
+	}
 }
