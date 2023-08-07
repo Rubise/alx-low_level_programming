403d62561bbe540f463e9d0b4d11b5a35c6742d7
Rubise <Rubisemamman@gmail.com>
2023-08-07 17:26:02 +0000

    New changes with betty fixed

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,12 +19,10 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	}
 	f = open(filename, O_WRONLY | O_APPEND);
-	
 	if (f == -1)
 	{
 		return (-1);
 	}
-	
 	if (text_content)
 	{
 		n = 0;
